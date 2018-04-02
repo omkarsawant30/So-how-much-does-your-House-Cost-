65f2ac1752c39fbdc6f4e2094df2a5e42f8eb27e
Omkar Sawant <37528117+omkarsawant30@users.noreply.github.com>
2018-04-02 12:30:36 +0530

    Update RealestateRF.R

--- RealestateRF.R
+++ RealestateRF.R
@@ -1,7 +1,11 @@
+##First we need to set the directory of the file's location in your computer and then read it so that R can download the data
 setwd("E:/DATA ANALYTICS JOURNEY/R Edvancer/PROJECT 1 REAL ESTATE")
 h_train=read.csv("housing_train.csv",stringsAsFactors = F)
 h_test=read.csv("housing_test.csv",stringsAsFactors = F)
 
+##You will need same set of vars on both train and test,its easier to manage that if you combine train and test
+##in the beginning and then separate them once you are done with data preparation
+##We'll fill test's response column with NAs.
 h_test$Price=NA
 
 h_train$data='train'
@@ -12,6 +16,8 @@ glimpse(h_all)
 
 head(h_all)
 
+##Next we'll create dummy variables for remaining categorical variables first by creating 
+##a new function CreateDummies and then using sapply for creating dummies
 CreateDummies=function(data,var,freq_cutoff=100){
   t=table(data[,var])
   t=t[t>freq_cutoff]
@@ -42,25 +48,32 @@ cat_cols
 cat_cols=cat_cols[!(cat_cols %in% c('data','Price'))]
 cat_cols
 
+# we are using frequency cutoff as 50, there is no magic number here,
+# lower cutoffs will simply result in more number of dummy variables
 for(col in cat_cols){
   h_all=CreateDummies(h_all,col,50)
 }
 
 glimpse(h_all)
 
+##we can go ahead and separate training and test data BUT first we check NA values
 h_all=h_all[!(is.na(h_all$Price) & h_all$data=="train"),]
+
 for(col in names(h_all)){
   if(sum(is.na(h_all[,col]))>0 & !(col %in% c("data","Price"))){
     h_all[is.na(h_all[,col]),col]=mean(h_all[h_all$data=='train',col],na.rm=T)
   }
 }
 
+##Lets separate our two data sets and remove the unnecessary columns
+## that we added while combining them
 h_train=h_all %>% filter(data=='train') %>% select(-data)
 h_test=h_all %>% filter(data=='test') %>% select(-data,-Price)
 
 any(is.na(h_train))
 any(is.na(h_test))
 
+#Using Random Forest model
 library(randomForest)
 fit = randomForest(Price~., data = h_train)
 
